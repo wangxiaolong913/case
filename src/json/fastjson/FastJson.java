c3f47a06c6981894e97dcf0949cac0d9997fa258
wangxiaolong913 <314072@qq.com>
2017-08-23 18:22:33 +0800

    update

--- src/json/fastjson/FastJson.java
+++ src/json/fastjson/FastJson.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 
 /**
  * @author wang.xiaolong
@@ -46,9 +47,11 @@ public class FastJson {
 		System.out.println(orderlist.get(0).getId());
 		System.out.println(orderlist.get(1).getOrderName());
 	}
+	
 
 	public static void main(String[] args) {
 		// beanToJson();
 		listToJson();
+
 	}
 }
